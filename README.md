8a5e0723efc218173216e723a3306589de1ab868
Jesse Bordoe <jesse.bordoe@gmail.com>
2022-12-20 01:07:57 +0100

    enable profiling of solutions with flag

--- README.md
+++ README.md
@@ -13,3 +13,12 @@ mix aoc --year=22 --day=2
 ```
 mix test
 ```
+
+## Profiling
+
+Use the `--profile` flag to generate trace output.
+
+```
+mix aoc --year=21 --day=18 --profile
+```
+This will generate an `fprof.trace` file. This can, for example, be converted into a callgrind (`erlgrind fprof.trace callgrind.out`) which can be visualised with [kcachegrind](https://kcachegrind.github.io/html/Home.html)
--- lib/mix/tasks/aoc.ex
+++ lib/mix/tasks/aoc.ex
@@ -10,13 +10,27 @@ defmodule Mix.Tasks.Aoc do
     {args, _} =
       System.argv()
       |> OptionParser.parse!(
-        strict: [day: :integer, year: :integer],
+        strict: [day: :integer, year: :integer, profile: :boolean],
         aliases: [d: :day, y: :year]
       )
 
     %{year: year, day: day} = Map.new(args)
+    profile = Keyword.get(args, :profile, false)
+
+
     input = stream_lines_from_file("priv/input/20#{year}/day_#{day}.txt")
     module = String.to_existing_atom("Elixir.Aoc#{year}.Day#{day}")
+
+    if profile do
+      :fprof.apply(&run_solution_code/2, [module, input])
+      :fprof.profile()
+      :fprof.analyse()
+    else
+      run_solution_code(module, input)
+    end
+  end
+
+  def run_solution_code(module, input) do
     [:solution1, :solution2]
     |> Enum.map(&(apply(module, &1, [input])))
     |> Enum.map(&IO.puts/1)
