97e7f25163f1ebf39ebafbf98624894ea5e17806
Jesse Bordoe <jesse.bordoe@gmail.com>
2022-12-25 17:18:18 +0100

    2022, day 18 part 2

--- lib/aoc22/day_18.ex
+++ lib/aoc22/day_18.ex
@@ -13,23 +13,32 @@ defmodule Aoc22.Day18 do
   end
 
   def solution2(input) do
-    cubes = parse(input)
-    area = surface_area(cubes)
-    interior_faces = 
+    cubes = Nx.pad(parse(input), 0, [{2,2,0}, {2,2,0}, {2,2,0}])
+    box = Nx.broadcast(1, cubes.shape)
+
+    inner = Nx.floor(Nx.window_mean(box, {3,3,3}, padding: :same))
+    outer = Nx.subtract(box, inner)
+    filter = Nx.subtract(box, cubes)
+
+    mold = flood(outer, filter)
+
+    Nx.subtract(box, mold)
+    |> surface_area()
+    |> Nx.sum()
+    |> Nx.to_number()
+  end
+
+  defp flood(prev, filter) do
+    expanded =
       [{3,1,1},{1,3,1},{1,1,3}]
       |> Enum.map(fn window_shape ->
-        Nx.window_sum(cubes, window_shape, padding: :same)
+        Nx.add(Nx.window_sum(prev, window_shape, padding: :same), prev)
       end)
       |> Enum.reduce(&Nx.add/2)
-      |> Nx.multiply(Nx.equal(0, cubes))
-      |> Nx.equal(6)
-      |> Nx.sum()
-      |> Nx.multiply(6)
+      |> Nx.ceil
+      |> Nx.min(filter)
 
-    area
-    |> Nx.sum()
-    |> Nx.subtract(interior_faces)
-    |> Nx.to_number()
+    if expanded == prev, do: prev, else: flood(expanded, filter)
   end
 
   defp surface_area(cubes) do
@@ -43,13 +52,12 @@ defmodule Aoc22.Day18 do
   end
 
   def parse(input) do
-    coords = 
+    coords =
       input
       |> Enum.map(fn line ->
         String.split(line, ",") |> Enum.map(&String.to_integer/1)
       end)
       |> Nx.tensor(names: [:x, :y])
-      |> IO.inspect
 
     shape =
       coords
